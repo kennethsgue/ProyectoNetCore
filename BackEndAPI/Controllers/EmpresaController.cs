13f124022f8da748296444d515fc961c69d69b3f
daylighted <45243229+daylighted@users.noreply.github.com>
2021-08-14 15:59:40 -0600

    Empresa
    Se agrega la tabla empresa al backend

---
+++ BackEndAPI/Controllers/EmpresaController.cs
@@ -0,0 +1,131 @@
+﻿using BackEnd.DAL;
+using BackEnd.Entities;
+using BackEndAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEndAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmpresaController : ControllerBase
+    {
+
+        private Empresa Convertir(EmpresaModel empresa)
+        {
+            return new Empresa
+            {
+                EmpresaId = empresa.EmpresaID,
+                Descripcion = empresa.Descripcion,
+                NombreEmpresa = empresa.NombreEmpresa,
+                Direccion = empresa.Direccion,
+                Clave = empresa.Clave,
+                Correo = empresa.Correo,
+                CedulaJuridica = empresa.CedulaJuridica,
+                Codigo = empresa.Codigo
+            };
+        }
+
+        // GET: api/<EmpleoController>
+        [HttpGet]
+        public JsonResult Get()
+        {
+            IEnumerable<Empresa> empresa;
+            using (UnidadDeTrabajo<Empresa> unidad = new
+                UnidadDeTrabajo<Empresa>(new EmpleosContext()))
+            {
+                empresa = unidad.genericDAL.GetAll();
+            }
+            return new JsonResult(empresa);
+        }
+
+        // GET api/<EmpleoController>/5
+        [HttpGet("{id}")]
+        public JsonResult Get(int id)
+        {
+            Empresa empresa;
+            using (UnidadDeTrabajo<Empresa> unidad = new
+                UnidadDeTrabajo<Empresa>(new EmpleosContext()))
+            {
+                empresa = unidad.genericDAL.Get(id);
+            }
+            return new JsonResult(empresa);
+        }
+
+        // POST api/<EmpleoController>
+        [HttpPost]
+        public bool Post([FromBody] EmpresaModel empresa)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return false;
+                }
+                using (UnidadDeTrabajo<Empresa> unidad = new UnidadDeTrabajo<Empresa>(new EmpleosContext()))
+                {
+                    unidad.genericDAL.Add(Convertir(empresa));
+                    unidad.Complete();
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        // PUT api/<EmpleoController>/5
+        [HttpPut("{id}")]
+        public bool Put([FromBody] EmpresaModel empresa)
+        {
+            bool result = false;
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return false;
+                }
+                using (UnidadDeTrabajo<Empresa> unidad = new UnidadDeTrabajo<Empresa>(new EmpleosContext()))
+                {
+                    unidad.genericDAL.Update(Convertir(empresa));
+                    result = unidad.Complete();
+                }
+                return result;
+            }
+            catch (Exception)
+            {
+                return result;
+            }
+        }
+
+        // DELETE api/<EmpleoController>/5
+        [HttpDelete("{id}")]
+        public bool Delete(int id)
+        {
+            try
+            {
+                Empresa empresa;
+                using (UnidadDeTrabajo<Empresa> unidad = new UnidadDeTrabajo<Empresa>(new EmpleosContext()))
+                {
+                    empresa = unidad.genericDAL.Get(id);
+                }
+                using (UnidadDeTrabajo<Empresa> unidad = new UnidadDeTrabajo<Empresa>(new EmpleosContext()))
+                {
+                    unidad.genericDAL.Remove(empresa);
+                    unidad.Complete();
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ BackEndAPI/Models/EmpresaModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackEndAPI.Models
+{
+    public class EmpresaModel
+    {
+        [Key]
+        public int EmpresaID { get; set; }
+        [Required]
+        public string Descripcion { get; set; }
+        public string NombreEmpresa { get; set; }
+        public string Direccion { get; set; }
+        public string Clave { get; set; }
+        public string Correo { get; set; }
+        public string CedulaJuridica { get; set; }
+        public int? Codigo { get; set; }
+    }
+}
